8139e73d2d41288e6da74c725744dd2ab46cfaca
DOMAIN-Noam <domainnoam@gmail.com>
2026-06-01 21:39:58 +0200

    Modifications

--- views/galerie.php
+++ views/galerie.php
@@ -26,6 +26,7 @@ else{
 	$tierlists = getPopulariteTierlists();
 }
 
+
 $tiers = ["S", "A", "B", "C", "D"];
 $couleurs = ["S"=>"#e72925", "A"=>"#e47e01", "B"=>"#b69b02", "C"=>"#4aaf4f", "D"=>"#1b7bcf"];
 
@@ -33,7 +34,7 @@ $couleurs = ["S"=>"#e72925", "A"=>"#e47e01", "B"=>"#b69b02", "C"=>"#4aaf4f", "D"
 
 <h1 class="titreGalerie">La Galerie des Tierlists de la Communauté</h1>
 
-<div class="bloc-recherche-galerie">
+<div class="rechercheGalerie">
     
     <?php mkForm("index.php", "GET"); //création du formulaire de recherche ?>
     <?php mkInput("hidden", "view", "galerie"); //on conserve le routage sur la galerie ?>
@@ -41,13 +42,13 @@ $couleurs = ["S"=>"#e72925", "A"=>"#e47e01", "B"=>"#b69b02", "C"=>"#4aaf4f", "D"
     <div class="row">
         <div class="col-md-3">
             <?php 
-            mkInput("text", "joueur", valider("joueur"), "class=\"form-control input-recherche\" placeholder=\"Joueur (Ex: Mbappé)\""); 
+            mkInput("text", "joueur", valider("joueur"), "class=\"form-control inputRecherche\" placeholder=\"Joueur (Ex: Mbappé)\""); 
             ?>
         </div>
         
         <div class="col-md-3">
             <?php 
-            mkInput("text", "pseudo", valider("pseudo"), "class=\"form-control input-recherche\" placeholder=\"Créateur\""); 
+            mkInput("text", "pseudo", valider("pseudo"), "class=\"form-control inputRecherche\" placeholder=\"Créateur\""); 
             ?>
         </div>
 
@@ -68,7 +69,7 @@ $couleurs = ["S"=>"#e72925", "A"=>"#e47e01", "B"=>"#b69b02", "C"=>"#4aaf4f", "D"
         
         <div class="col-md-2">
             <?php 
-            mkInput("submit", "btn_recherche", "Filtrer", "class=\"btn btn-primary btn-block bouton-filtrer\""); 
+            mkInput("submit", "btn_recherche", "Filtrer", "class=\"btn btn-primary btn-block boutonFiltrer\""); 
             ?>
         </div>
     </div>
@@ -82,9 +83,10 @@ $couleurs = ["S"=>"#e72925", "A"=>"#e47e01", "B"=>"#b69b02", "C"=>"#4aaf4f", "D"
     if ($tierlists != false) {
         foreach($tierlists as $t){
             $idTierlist = $t['id_tierlist']; 
+            $infosTierlist = getTierlistParId($idTierlist);
             $contenuParTier = getContenuTierlist($idTierlist);  
-            echo "<div class=\"col-md-3 colonne-carte\">";
-            echo "<div class=\"carte-tierlist\">";
+            echo "<div class=\"col-md-3 colonneCarte\">";
+            echo "<div class=\"carteTierlist\">";
             echo "<a href=\"index.php?view=detail_tierlist&idTierlist=$idTierlist\">";
             //On affiche la tierlist en petit comme dans la vue detail_tierlist
             echo "<div class=\"detailTierlist\">";
@@ -106,36 +108,25 @@ $couleurs = ["S"=>"#e72925", "A"=>"#e47e01", "B"=>"#b69b02", "C"=>"#4aaf4f", "D"
                         echo "</div>";
                     }
                 }
-                echo "</div>";
-                echo "</div>";
+                echo "</div></div>";
             }
+            echo "</div></a>";
+            echo "<div class=\"infosTierlist\">";
+            echo "<div class=\"texteLikes\">♡";
+            echo $infosTierlist["nb_likes"];
             echo "</div>";
-            echo "</a>";
-            echo "<div class=\"infos-tierlist\">";
-            echo "<div class=\"texte-likes\">";
-            echo "♡";
-            if (valider("nb_likes")){
-                echo $tl['nb_likes'];
-            }
-            else{
-                echo "0";
-            }
-            echo "</div>";
-            echo "<a href=\"index.php?view=detail_tierlist&idTierlist=$idTierlist\" class=\"lien-titre\">";
-            echo htmlspecialchars($t['titre']); 
-            echo "</a>";
-            echo "</div>";
+            echo "<a href=\"index.php?view=detail_tierlist&idTierlist=$idTierlist\" class=\"lienTitre\">";
+            echo htmlspecialchars($t["titre"]); 
+            echo "</a></div>";
             
-            echo "<div class=\"texte-auteur\">";
-            echo "Par <strong>" . htmlspecialchars($t['createur']) . "</strong>";
-            echo "</div>";
-            echo "</div>";
-            echo "</div>";
+            echo "<div class=\"texteAuteur\">";
+            echo "Par <strong>" . htmlspecialchars($t['createur']) . "</strong></div>";
+            echo "</div></div>";
         }     
     }
     else{
         //message d'alerte si aucun résultat n'est retourné par le modèle
-        echo "<div class=\"col-md-12 text-center message-vide\">Aucune tierlist trouvée pour cette recherche.</div>";
+        echo "<div class=\"col-md-12 text-center messageVide\">Aucune tierlist trouvée pour cette recherche.</div>";
     }
     ?>
 </div>
